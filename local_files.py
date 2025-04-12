7b7efdffd0b87b88639659e5130e5989156da395
demch <md2710@ukr.net>
2025-04-12 19:02:45 +0300

    new1

---
+++ local_files.py
@@ -0,0 +1,13 @@
+import requests
+
+url = 'https://moemisto.ua/img/cache/blog_show_photo/blog/0004/75/fe2a5835d6d62a84d64cc357061c8186a244a1a8.jpeg'
+
+responce = requests.get(url)
+
+with open('spring.jpeg', mode="wb") as file:
+    content = responce.content
+    content += b'327637 Moks'
+    file.write(content)
+with open('spring.jpeg', mode='rb') as f:
+    print(f.read())
+pass
